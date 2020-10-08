dd01fb3928f5ee3d2d9f51649bb8019c8ed4d141
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-10-08 13:25:54 -0400

    ADD docker build config

--- fuzz/build.sh
+++ fuzz/build.sh
@@ -5,17 +5,6 @@ set -e
 cd $(dirname $(realpath $0))
 
 mkdir -p build bin
-cd build
 
-rm -rf CMakeFiles/
-rm -f CMakeCache.txt
-rm -f cmake_install.cmake
-rm -f Makefile
-
-export CC=$(which clang)
-export CXX=$(which clang++)
-
-cmake ..
-make -j $(nproc)
-
-cp pwnbois_fuzz ../bin
+cmake -B build -G Ninja .
+ninja -C build
